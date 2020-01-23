1669a27d53597a2139a836d7ef10daf9597dfb8b
kmj91 <lethita0302@gmail.com>
2020-01-23 23:57:01 +0900

    파티클 컬러 바꾸기 1

--- Catherine/Assets/Scripts/CubeMovement.cs
+++ Catherine/Assets/Scripts/CubeMovement.cs
@@ -73,6 +73,19 @@ public class CubeMovement : MonoBehaviour
         actionDelay = 0f;
         // 중력
         isGravity = true;
+
+
+        if (gameObject.CompareTag("NormalCube"))
+        {
+            var col = RB_SparksEffect.colorOverLifetime;
+            col.enabled = true;
+
+            Gradient grad = new Gradient();
+            grad.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.blue, 0.0f), new GradientColorKey(Color.red, 1.0f) },
+                        new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(0.0f, 1.0f) });
+
+            col.color = new ParticleSystem.MinMaxGradient(grad);
+        }
     }
 
     
