635193216f7c9d6d02bcfc11418cc120c9db4659
kmj91 <lethita0302@gmail.com>
2020-02-16 23:59:49 +0900

    deque 구현

--- Catherine/Assets/Scripts/UnityDeque.cs
+++ Catherine/Assets/Scripts/UnityDeque.cs
@@ -14,17 +14,18 @@ namespace UnityDequeScript
         // 멤버 변수
         //--------------------------------
 
-        public int Count { get; }   // 사용중인 버퍼 사이즈
+        public int Count { get; private set; }   // 사용중인 버퍼 사이즈
 
         private int m_front;        // 프론트
         private int m_rear;         // 리어
+        private int m_iBufferSize;  // 버퍼 사이즈
         private T[] m_deque;        // 버퍼
 
         //--------------------------------
         // 상수
         //--------------------------------
 
-        private const int DEFAULT_SIZE = 10;
+        private const int DEFAULT_SIZE = 11;
 
 
         //--------------------------------
@@ -34,11 +35,17 @@ namespace UnityDequeScript
         public UnityDeque()
         {
             m_deque = new T[DEFAULT_SIZE];
+            m_iBufferSize = DEFAULT_SIZE;
+            m_front = 0;
+            m_rear = 0;
         }
 
         public UnityDeque(int iSize)
         {
             m_deque = new T[iSize];
+            m_iBufferSize = iSize;
+            m_front = 0;
+            m_rear = 0;
         }
 
         ~UnityDeque()
@@ -49,43 +56,148 @@ namespace UnityDequeScript
         // 앞
         public bool Push_Front(T data)
         {
+            // 버퍼가 꽉찼나
+            if (m_iBufferSize - 1 == Count)
+            {
+                // 꽉참
+                return false;
+            }
+
+            if (m_front == 0)
+            {
+                // 프론트 한칸 왼쪽 이동
+                m_front = m_iBufferSize - 1;
+                // 데이터 입력
+                m_deque[m_front] = data;
+                ++Count;
+            }
+            else
+            {
+                // 프론트 한칸 왼쪽 이동
+                m_front = m_front - 1;
+                // 데이터 입력
+                m_deque[m_front] = data;
+                ++Count;
+            }
 
             return true;
         }
 
         public bool Push_Back(T data)
         {
+            // 버퍼가 꽉찼나
+            if (m_iBufferSize - 1 == Count)
+            {
+                // 꽉참
+                return false;
+            }
+
+            // 데이터 입력
+            m_deque[m_rear] = data;
+            ++Count;
+            // 리어 한칸 오른쪽 이동
+            m_rear = (m_rear + 1) % m_iBufferSize;
 
             return true;
         }
 
         public T Pop_Front()
         {
+            int iPopFront;
 
-            return m_deque[m_front];
+            // 버퍼가 비었나
+            if (Count == 0)
+            {
+                // 빔
+                return default(T);
+            }
+
+            // 출력할 프론트 인덱스
+            iPopFront = m_front;
+            // 프론트 한칸 오른쪽 이동
+            m_front = (m_front + 1) % m_iBufferSize;
+
+            return m_deque[iPopFront];
         }
 
         public T Pop_Back()
         {
+            // 버퍼가 비었나
+            if (Count == 0)
+            {
+                // 빔
+                return default(T);
+            }
+
+            if (m_rear == 0)
+            {
+                // 출력할 리어 인덱스
+                m_rear = m_iBufferSize - 1;
+            }
+            else
+            {
+                // 출력할 리어 인덱스
+                --m_rear;
+            }
 
             return m_deque[m_rear];
         }
 
         public T Peek_Front()
         {
+            // 버퍼가 비었나
+            if (Count == 0)
+            {
+                // 빔
+                return default(T);
+            }
 
             return m_deque[m_front];
         }
 
         public T Peek_Back()
         {
+            int iPopRear;
 
-            return m_deque[m_rear];
+            // 버퍼가 비었나
+            if (Count == 0)
+            {
+                // 빔
+                return default(T);
+            }
+
+            if (m_rear == 0)
+            {
+                // 출력할 리어 인덱스
+                iPopRear = m_iBufferSize - 1;
+            }
+            else
+            {
+                // 출력할 리어 인덱스
+                iPopRear = m_rear - 1;
+            }
+
+            return m_deque[iPopRear];
         }
 
         public void Clear()
         {
+            Count = 0;
+            m_front = 0;
+            m_rear = 0;
+        }
 
+        public bool isEmpty()
+        {
+            //if (m_front == m_rear)
+            if (Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 
