580fdd80a63e09bbffe6442375ab90caaed59641
kmj91 <lethita0302@gmail.com>
2020-03-11 06:27:45 +0900

    오브젝트 지우기 추가

--- Catherine/Assets/Scripts/MapToolManager.cs
+++ Catherine/Assets/Scripts/MapToolManager.cs
@@ -22,8 +22,9 @@ public class MapToolManager : MonoBehaviour
 
     private int height;                             // 오브젝트 배열의 Y축
     private int selectElement;                      // 선택된 요소
-    private bool mouseFlag;
+    private Vector3 MouseFieldPoint;                // 현제 맵툴 필드위에서의 마우스 위치
     private bool controlToggle;                     // 컨트롤 키 토글
+    private bool onFieldMouse;                      // 맵툴 필드 위에 마우스가 있나 없나
 
     private Camera screenCamera;                    // 메인 카메라
     private GameObject player;                      // 선택 영역 표시용 플레이어
@@ -50,8 +51,6 @@ public class MapToolManager : MonoBehaviour
         {
             case MenuElementType.EMPTY:
                 // 빈 상태
-                // Update에서 마우스 처리 false
-                mouseFlag = false;
                 // 선택 영역
                 selectField.SetActive(false);
                 // 없음
@@ -62,8 +61,6 @@ public class MapToolManager : MonoBehaviour
                 break;
             case MenuElementType.PLAYER:
                 // 플레이어
-                // Update에서 마우스 처리 true
-                mouseFlag = true;
                 // 선택 영역
                 selectField.SetActive(true);
                 // 플레이어 보이기
@@ -74,8 +71,6 @@ public class MapToolManager : MonoBehaviour
                 break;
             case MenuElementType.NORMAL_CUBE:
                 // 노말 큐브
-                // Update에서 마우스 처리 true
-                mouseFlag = true;
                 // 선택 영역
                 selectField.SetActive(true);
                 // 노말 큐브 보이기
@@ -86,8 +81,6 @@ public class MapToolManager : MonoBehaviour
                 break;
             case MenuElementType.ICE_CUBE:
                 // 얼음 큐브
-                // Update에서 마우스 처리 true
-                mouseFlag = true;
                 // 선택 영역
                 selectField.SetActive(true);
                 // 얼음 큐브 보이기
@@ -137,7 +130,8 @@ public class MapToolManager : MonoBehaviour
             { KeyCode.W, Up},
             { KeyCode.UpArrow, Up},
             { KeyCode.S, Down},
-            { KeyCode.DownArrow, Down}
+            { KeyCode.DownArrow, Down},
+            { KeyCode.LeftControl, LeftControlDown}
         };
 
         // 키 오토
@@ -147,13 +141,14 @@ public class MapToolManager : MonoBehaviour
             { KeyCode.LeftArrow, Left},
             { KeyCode.D, Right},
             { KeyCode.RightArrow, Right},
-            { KeyCode.LeftControl, LeftControl}
+            { KeyCode.Mouse0, MouseLeftClickAuto},
+            { KeyCode.Mouse1, MouseRightClickAuto}
         };
 
         // 키 업
         keyUp = new Dictionary<KeyCode, Action>
         {
-            { KeyCode.LeftControl, LeftControl}
+            { KeyCode.LeftControl, LeftControlUp}
         };
 
         // 조합 키
@@ -168,11 +163,8 @@ public class MapToolManager : MonoBehaviour
     private void Update()
     {
 
-
-        if (mouseFlag)
-        {
-            MouseProc();
-        }
+        // 마우스 처리
+        MouseProc();
 
         // 키처리
         KeyProc();
@@ -183,13 +175,6 @@ public class MapToolManager : MonoBehaviour
     private void MouseProc()
     {
         RaycastHit rayHit;          // 레이 충돌한 물체
-        Vector3 createPoint;
-
-        // 만들게 없다
-        if (createObject == null)
-        {
-            return;
-        }
 
         // 화면 마우스 좌표
         var screenMouse = Input.mousePosition;
@@ -199,21 +184,36 @@ public class MapToolManager : MonoBehaviour
         // 레이
         if (!Physics.Raycast(world, screenCamera.transform.forward, out rayHit, 20f, layerMaskFloor))
         {
+            selectField.SetActive(false);
+            onFieldMouse = false;
             return;
+        }
+        else {
+            onFieldMouse = true;
         }
 
         Debug.DrawRay(world, screenCamera.transform.forward * 20f, Color.red);
 
         // 반올림, 올림
-        createPoint.x = Mathf.Round(rayHit.point.x);
-        createPoint.y = Mathf.Ceil(rayHit.point.y);
-        createPoint.z = Mathf.Round(rayHit.point.z);
+        MouseFieldPoint.x = Mathf.Round(rayHit.point.x);
+        MouseFieldPoint.y = Mathf.Ceil(rayHit.point.y);
+        MouseFieldPoint.z = Mathf.Round(rayHit.point.z);
 
-        selectField.transform.position = createPoint;
+        // 아무것도 선택 안한 상태임
+        if (selectElement == (int)MenuElementType.EMPTY)
+        {
+            selectField.SetActive(false);
+            return;
+        }
+        else
+        {
+            selectField.SetActive(true);
+        }
 
-        var position = selectField.transform.position;
+        selectField.transform.position = MouseFieldPoint;
+
         // 비어있음
-        if (arrMapObject[(int)position.y, (int)position.z, (int)position.x].objectType == MenuElementType.EMPTY)
+        if (arrMapObject[(int)MouseFieldPoint.y, (int)MouseFieldPoint.z, (int)MouseFieldPoint.x].objectType == MenuElementType.EMPTY)
         {
             // 만들 수 있음 초록색
             selectField.GetComponent<MeshRenderer>().material.color = new Color(0, 200, 0, 0.1f);
@@ -229,8 +229,6 @@ public class MapToolManager : MonoBehaviour
     // 키 처리
     private void KeyProc()
     {
-        //ObjectData objectData;
-
         // 키 다운
         if (Input.anyKeyDown)
         {
@@ -270,148 +268,11 @@ public class MapToolManager : MonoBehaviour
         // 키 업
         foreach (var pair in keyUp)
         {
-            if (Input.GetKey(pair.Key))
+            if (Input.GetKeyUp(pair.Key))
             {
                 pair.Value();
             }
         }
-
-
-        /*
-        // 마우스 왼쪽 클릭
-        if (Input.GetMouseButton(0))
-        {
-            if (createObject == null)
-            {
-                return;
-            }
-
-            var position = selectField.transform.position;
-
-            // 오브젝트 생성
-            switch ((MenuElementType)selectElement)
-            {
-                case MenuElementType.PLAYER:
-                    // 플레이어
-
-                    if (arrMapObject[(int)position.y, (int)position.z, (int)position.x].objectType != MenuElementType.EMPTY)
-                    {
-                        break;
-                    }
-
-                    position = createObject.transform.position;
-
-                    objectData.objectType = MenuElementType.PLAYER;
-                    objectData.gameObject = Instantiate<GameObject>(playerPrefab);
-                    objectData.gameObject.name = "player [" + (int)position.y + ", " + (int)position.z + ", " + (int)position.x + "]";
-                    objectData.color = Color.black;
-                    objectData.gameObject.transform.position = position;
-                    objectData.gameObject.transform.parent = gameStage.transform;
-
-                    arrMapObject[(int)position.y, (int)position.z, (int)position.x] = objectData;
-                    break;
-                case MenuElementType.NORMAL_CUBE:
-                    // 노말 큐브
-
-                    if (arrMapObject[(int)position.y, (int)position.z, (int)position.x].objectType != MenuElementType.EMPTY)
-                    {
-                        break;
-                    }
-
-                    objectData.objectType = MenuElementType.NORMAL_CUBE;
-                    objectData.gameObject = Instantiate<GameObject>(normalCubePrefab);
-                    objectData.gameObject.name = "NormalCube [" + (int)position.y + ", " + (int)position.z + ", " + (int)position.x + "]";
-                    objectData.color = objectData.gameObject.GetComponent<MeshRenderer>().material.color;
-                    objectData.gameObject.transform.position = position;
-                    objectData.gameObject.transform.parent = gameStage.transform;
-
-                    arrMapObject[(int)position.y, (int)position.z, (int)position.x] = objectData;
-                    break;
-                case MenuElementType.ICE_CUBE:
-                    // 얼음 큐브
-
-                    if (arrMapObject[(int)position.y, (int)position.z, (int)position.x].objectType != MenuElementType.EMPTY)
-                    {
-                        break;
-                    }
-
-                    objectData.objectType = MenuElementType.ICE_CUBE;
-                    objectData.gameObject = Instantiate<GameObject>(iceCubePrefab);
-                    objectData.gameObject.name = "IceCube [" + (int)position.y + ", " + (int)position.z + ", " + (int)position.x + "]";
-                    objectData.color = objectData.gameObject.GetComponent<MeshRenderer>().material.color;
-                    objectData.gameObject.transform.position = position;
-                    objectData.gameObject.transform.parent = gameStage.transform;
-
-                    arrMapObject[(int)position.y, (int)position.z, (int)position.x] = objectData;
-                    break;
-            }
-        }
-
-        // W 키
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            if (height + 1 < 100)
-            {
-                ++height;
-                guideLine.transform.position = new Vector3(0f, height, 0f);
-                // 카메라 위로
-                screenCamera.transform.position = screenCamera.transform.position + new Vector3(0f, 1f, 0f);
-                // 현제 층과 위 아래 오브젝트 색상 변경
-                updateColor();
-            }
-        }
-
-        // S 키
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            if (height - 1 >= 0)
-            {
-                --height;
-                guideLine.transform.position = new Vector3(0f, height, 0f);
-                // 카메라 아래로
-                screenCamera.transform.position = screenCamera.transform.position + new Vector3(0f, -1f, 0f);
-                // 현제 층과 위 아래 오브젝트 색상 변경
-                updateColor();
-            }
-        }
-
-        // 왼쪽 키 누르고 있음
-        if (Input.GetKey(KeyCode.A))
-        {
-            var angle = cameraTarget.transform.eulerAngles;
-            angle.y = angle.y - 1f;
-            cameraTarget.transform.eulerAngles = angle;
-        }
-
-        // 오른쪽 키 누르고 있음
-        if (Input.GetKey(KeyCode.D))
-        {
-            var angle = cameraTarget.transform.eulerAngles;
-            angle.y = angle.y + 1f;
-            cameraTarget.transform.eulerAngles = angle;
-        }
-
-        // 파일 내보내기
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            Stream ws = new FileStream("a.dat", FileMode.Create);
-            BinaryFormatter serializer = new BinaryFormatter();
-
-            serializer.Serialize(ws, arrMapObject);
-            ws.Close();
-
-            //---------------------------------
-            // 전달할 다른 정보 생각해보기
-            //---------------------------------
-            Debug.Log("저장");
-        }
-
-        // 파일 불러오기
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            Load();
-        }
-        */
     }
 
 
@@ -525,17 +386,130 @@ public class MapToolManager : MonoBehaviour
         cameraTarget.transform.eulerAngles = angle;
     }
 
-    // 왼쪽 컨트롤 키
-    private void LeftControl()
+    // 마우스 왼쪽 클릭 오토
+    private void MouseLeftClickAuto()
     {
-        if (controlToggle)
+        ObjectData objectData;
+        Vector3 position = MouseFieldPoint;
+
+        // 오브젝트 생성
+        switch ((MenuElementType)selectElement)
         {
-            controlToggle = false;
+            case MenuElementType.EMPTY:
+
+                break;
+            case MenuElementType.PLAYER:
+                // 플레이어
+
+                // 마우스가 필드위에서 벗어남
+                if (!onFieldMouse)
+                {
+                    return;
+                }
+
+                // 해당 위치가 비어있지 않음
+                if (arrMapObject[(int)position.y, (int)position.z, (int)position.x].objectType != MenuElementType.EMPTY)
+                {
+                    return;
+                }
+
+                objectData.objectType = MenuElementType.PLAYER;
+                objectData.gameObject = Instantiate<GameObject>(playerPrefab);
+                objectData.gameObject.name = "player [" + (int)position.y + ", " + (int)position.z + ", " + (int)position.x + "]";
+                objectData.color = Color.black;
+                // 오브젝트 마다 중심점이 다름
+                // 각자 맞게 배치됨
+                objectData.gameObject.transform.position = createObject.transform.position;
+                objectData.gameObject.transform.parent = gameStage.transform;
+
+                arrMapObject[(int)position.y, (int)position.z, (int)position.x] = objectData;
+                return;
+            case MenuElementType.NORMAL_CUBE:
+                // 노말 큐브
+
+                // 마우스가 필드위에서 벗어남
+                if (!onFieldMouse)
+                {
+                    return;
+                }
+
+                // 해당 위치가 비어있지 않음
+                if (arrMapObject[(int)position.y, (int)position.z, (int)position.x].objectType != MenuElementType.EMPTY)
+                {
+                    return;
+                }
+
+                objectData.objectType = MenuElementType.NORMAL_CUBE;
+                objectData.gameObject = Instantiate<GameObject>(normalCubePrefab);
+                objectData.gameObject.name = "NormalCube [" + (int)position.y + ", " + (int)position.z + ", " + (int)position.x + "]";
+                objectData.color = objectData.gameObject.GetComponent<MeshRenderer>().material.color;
+                // 오브젝트 마다 중심점이 다름
+                // 각자 맞게 배치됨
+                objectData.gameObject.transform.position = createObject.transform.position;
+                objectData.gameObject.transform.parent = gameStage.transform;
+
+                arrMapObject[(int)position.y, (int)position.z, (int)position.x] = objectData;
+                return;
+            case MenuElementType.ICE_CUBE:
+                // 얼음 큐브
+
+                // 마우스가 필드위에서 벗어남
+                if (!onFieldMouse)
+                {
+                    return;
+                }
+
+                // 해당 위치가 비어있지 않음
+                if (arrMapObject[(int)position.y, (int)position.z, (int)position.x].objectType != MenuElementType.EMPTY)
+                {
+                    return;
+                }
+
+                objectData.objectType = MenuElementType.ICE_CUBE;
+                objectData.gameObject = Instantiate<GameObject>(iceCubePrefab);
+                objectData.gameObject.name = "IceCube [" + (int)position.y + ", " + (int)position.z + ", " + (int)position.x + "]";
+                objectData.color = objectData.gameObject.GetComponent<MeshRenderer>().material.color;
+                // 오브젝트 마다 중심점이 다름
+                // 각자 맞게 배치됨
+                objectData.gameObject.transform.position = createObject.transform.position;
+                objectData.gameObject.transform.parent = gameStage.transform;
+
+                arrMapObject[(int)position.y, (int)position.z, (int)position.x] = objectData;
+                return;
         }
-        else
+    }
+
+    // 마우스 오른쪽 클릭 오토
+    private void MouseRightClickAuto()
+    {
+        Vector3 position = MouseFieldPoint;
+
+        // 마우스가 필드위에서 벗어남
+        if (!onFieldMouse)
         {
-            controlToggle = true;
+            return;
         }
+
+        // 해당 위치가 비어있음
+        if (arrMapObject[(int)position.y, (int)position.z, (int)position.x].objectType == MenuElementType.EMPTY)
+        {
+            return;
+        }
+
+        arrMapObject[(int)position.y, (int)position.z, (int)position.x].objectType = MenuElementType.EMPTY;
+        Destroy(arrMapObject[(int)position.y, (int)position.z, (int)position.x].gameObject);
+    }
+
+    // 왼쪽 컨트롤 키 다운
+    private void LeftControlDown()
+    {
+        controlToggle = true;
+    }
+
+    // 왼쪽 컨트롤 키 업
+    private void LeftControlUp()
+    {
+        controlToggle = false;
     }
 
     // 세이브
